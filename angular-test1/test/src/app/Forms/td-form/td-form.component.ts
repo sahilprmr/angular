fa5d0c871af9de65e266933347615780163c35b6
sahil <sahil@gmail.com>
2023-02-22 14:22:03 +0530

    --> Updation Done !!!     ->Reactive Form     ->Query routing     ->Storage (pending)

--- angular-test1/test/src/app/Forms/td-form/td-form.component.ts
+++ angular-test1/test/src/app/Forms/td-form/td-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
+import { OutletContext } from '@angular/router';
+import { DataService } from 'src/app/data.service';
+
 
 @Component({
   selector: 'app-td-form',
@@ -6,24 +9,30 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./td-form.component.css']
 })
 export class TdFormComponent {
-genders = ['Female','Male'];
-fullname = '';
-fname = '';
-mname = '';
-lname = '';
-defaultOption = 'Dancing';
-// @Input()
-// company =['TechExtensor','Google','Facebook','Simform','Yahoo','Instagram'];
-onSubmit(){
-  
-}
-input_fullname(fname:any,mname:any,lname:any){
-  this.fname = fname;
-  this.lname = lname;
-  this.mname = mname;
-  this.fullname= fname.value +mname.value + lname.value;
-  console.log(this.fname);
-  
 
-}
+  genders = ['Female', 'Male'];
+  fullname = '';
+  fname = '';
+  mname = '';
+  lname = '';
+  defaultOption = 'Dancing';
+  // @Input()
+  // company =['TechExtensor','Google','Facebook','Simform','Yahoo','Instagram'];
+  onSubmit() {
+
+  }
+  constructor(private formdata: DataService) { }
+  input_fullname(fname: any, mname: any, lname: any) {
+    this.fname = fname;
+    this.lname = lname;
+    this.mname = mname;
+    this.fullname = fname.value + ' ' + mname.value + ' ' + lname.value;
+    console.log(this.fname);
+  }
+  OnClear() {
+    this.fullname = '';
+    this.fname = '';
+    this.mname = '';
+    this.lname = '';
+  }
 }
